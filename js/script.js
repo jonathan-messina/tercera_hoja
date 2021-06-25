2987a046c9c6b570fc45f54c36d48976acd6809d
johnjohnpixel <a.jonathan.messina@gmail.com>
2021-06-25 16:27:34 -0300

    Added all files from localstorage

---
+++ js/script.js
@@ -0,0 +1,112 @@
+
+//Aplica fadeInLeft a los botones next,prev y al primer dot//
+
+document.getElementById('next').addEventListener('click',function next() {
+  var c = document.getElementsByClassName('img-slider-1');
+  for (var i = 0; i < c.length; i++) {
+  c[i].classList.add('fadeInLeft');
+  }
+})
+
+
+document.getElementById('prev').addEventListener('click',function prev() {
+var g = document.getElementsByClassName('img-slider-1');
+for (var i = 0; i < g.length; i++) {
+g[i].classList.add('fadeInLeft');
+}
+})
+
+document.getElementById('dot1').addEventListener('click',function prev() {
+  var g = document.getElementsByClassName('img-slider-1');
+  for (var i = 0; i < g.length; i++) {
+  g[i].classList.add('fadeInLeft');
+  }
+  })
+
+//Aplica fadeInLeft a los botones next,prev y al primer dot//
+
+//                   logica de slider                  //
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//                   logica de slider                   //
+
+//top button//
+
+//Get the button://
+mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+//top button//
+
+//menu hamburguesa//
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+//menu hamburguesa//
